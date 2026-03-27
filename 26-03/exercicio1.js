5b3c1319776fd0eb675e92dfe8c4662ef7f9627e
Erick-FSR <erick.f.rodrigues0@gmail.com>
2026-03-26 21:32:00 -0300

    Add files via upload

---
+++ 26-03/exercicio1.js
@@ -0,0 +1,10 @@
+//Variáveis
+let a = true
+let b = false
+let c = true
+
+//Resultado
+console.log(a||b)
+console.log(b||c)
+console.log(a||c)
+console.log(a||b||c)
---
+++ 26-03/exercicio2.js
@@ -0,0 +1,14 @@
+//Variáveis
+const nome = "Érick Fernando"
+let anoDeNascimento = 2006
+let anoAtual = 2026
+let result = anoAtual - anoDeNascimento
+
+
+
+//Resultados
+console.log("======================SISTEMA DE CÁLCULO=============")
+console.log("Nome:", nome)
+console.log("Idade:", result)
+console.log("É maior de idade?", result >= 18)
+console.log("Idade em 2050:", 2050 - anoAtual)
